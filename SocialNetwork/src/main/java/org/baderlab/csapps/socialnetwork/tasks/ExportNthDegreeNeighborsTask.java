033120e11147f31f256f4bbd8904cef1d7316d69
Victor Kofia <victor.kofia@gmail.com>
2015-07-08 09:41:29 -0400

    TaskMonitor for ExportNthDegreeNeighborsTask now shows progress. Fixed bug that was causing 'null' authors to be included in the final list.

--- SocialNetwork/src/main/java/org/baderlab/csapps/socialnetwork/tasks/ExportNthDegreeNeighborsTask.java
+++ SocialNetwork/src/main/java/org/baderlab/csapps/socialnetwork/tasks/ExportNthDegreeNeighborsTask.java
@@ -3,6 +3,7 @@ package org.baderlab.csapps.socialnetwork.tasks;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.baderlab.csapps.socialnetwork.CytoscapeUtilities;
@@ -14,17 +15,21 @@ import org.cytoscape.work.AbstractTask;
 import org.cytoscape.work.TaskMonitor;
 
 /**
- * Task for exporting a csv file containing the nth degree neighbors of
- * every node in an existing Cytoscape network.
+ * Task for exporting a csv file containing the nth degree neighbors of every
+ * node in an existing Cytoscape network.
  * 
  * @author Victor Kofia
  */
 public class ExportNthDegreeNeighborsTask extends AbstractTask {
-    
+
     private static final Logger logger = Logger.getLogger(ExportNthDegreeNeighborsTask.class.getName());
-    
+
     private SocialNetworkAppManager appManager = null;
-    
+    private TaskMonitor taskMonitor = null;
+    private int currentSteps = 0;
+    private int totalSteps = 0;
+    private double progress = 0.0;
+
     /**
      * Constructor for {@link ExportNthDegreeNeighborsTask}
      * 
@@ -34,27 +39,58 @@ public class ExportNthDegreeNeighborsTask extends AbstractTask {
         this.appManager = appManager;
     }
 
-    /* (non-Javadoc)
+    /**
+     * Return the ordinal for an integer value
+     * 
+     * @param int value
+     * @return String ordinal
+     */
+    public static String getOrdinalFor(int value) {
+        int hundredRemainder = value % 100;
+        int tenRemainder = value % 10;
+        if (hundredRemainder - tenRemainder == 10) {
+            return "th";
+        }
+        switch (tenRemainder) {
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see org.cytoscape.work.AbstractTask#run(org.cytoscape.work.TaskMonitor)
      */
     @Override
     public void run(TaskMonitor taskMonitor) throws Exception {
+        this.taskMonitor = taskMonitor;
         CyNetwork cyNetwork = this.appManager.getNeighborListNetwork();
         int degree = this.appManager.getNeighborListDegree();
+        String message = String.format("Exporting %d%s degree neighbors ...", degree, getOrdinalFor(degree));
+        setProgressMonitor(message, cyNetwork.getNodeCount());
         File selectedFolder = this.appManager.getNeighborListFolder();
         String attr = this.appManager.getNeighborListAttribute();
         exportNeighborsToCSV(cyNetwork, degree, selectedFolder, attr);
     }
-    
+
     /**
      * Export the nth degree neighbors of the specified network to CSV
      * 
      * @param CyNetwork cyNetwork
      * @param int degree
+     * @param File selectedFolder
+     * @param String attr
      */
     private void exportNeighborsToCSV(CyNetwork cyNetwork, int degree, File selectedFolder, String attr) {
         try {
-            String fileName = "/neighbor_list.csv";
+            String fileName = "/neighbor_list.csv", attrValue = null;
             FileWriter writer = new FileWriter(selectedFolder.getAbsolutePath() + fileName);
             writer.append("Author");
             writer.append(',');
@@ -63,10 +99,14 @@ public class ExportNthDegreeNeighborsTask extends AbstractTask {
             if (!attr.equalsIgnoreCase("N/A")) {
                 for (CyNode node : cyNetwork.getNodeList()) {
                     // column is assumed to be of type String
-                    writer.append(cyNetwork.getDefaultNodeTable().getRow(node.getSUID()).get(attr, String.class));
-                    writer.append(',');
-                    writeNthDegreeNode(node, cyNetwork, writer, attr, degree);
-                    writer.append('\n');
+                    attrValue = cyNetwork.getDefaultNodeTable().getRow(node.getSUID()).get(attr, String.class);
+                    if (attrValue != null) {
+                        writer.append(attrValue);
+                        writer.append(',');
+                        writeNthDegreeNode(node, cyNetwork, writer, attr, degree);
+                        writer.append('\n');
+                        updateProgress();
+                    }
                 }
             }
             writer.flush();
@@ -76,7 +116,7 @@ public class ExportNthDegreeNeighborsTask extends AbstractTask {
             CytoscapeUtilities.notifyUser("IOException. Unable to save csv file");
         }
     }
-    
+
     /**
      * Write the nth degree neighbors of {@code node} into a text file
      * 
@@ -100,5 +140,43 @@ public class ExportNthDegreeNeighborsTask extends AbstractTask {
             writeNthDegreeNode(neighbour, network, writer, attr, depth - 1);
         }
     }
-    
+
+    /**
+     * Set progress monitor
+     *
+     * @param TaskMonitor taskMonitor
+     * @param String taskName
+     * @param int totalSteps
+     */
+    private void setProgressMonitor(String taskName, int totalSteps) {
+        this.taskMonitor.setTitle(taskName);
+        this.taskMonitor.setProgress(0.0);
+        this.currentSteps = 0;
+        this.totalSteps = totalSteps;
+    }
+
+    /**
+     * Return progress as a percentage
+     *
+     * @param Double progress
+     * @return String percentage
+     */
+    private String toPercent(double progress) {
+        progress = progress * 100;
+        DecimalFormat df = new DecimalFormat("00");
+        return df.format(progress) + "%";
+    }
+
+    /**
+     * Update progress monitor
+     *
+     * @param int currentSteps
+     */
+    private void updateProgress() {
+        this.currentSteps += 1;
+        this.progress = (double) this.currentSteps / this.totalSteps;
+        this.taskMonitor.setStatusMessage("Complete: " + toPercent(this.progress));
+        this.taskMonitor.setProgress(this.progress);
+    }
+
 }
